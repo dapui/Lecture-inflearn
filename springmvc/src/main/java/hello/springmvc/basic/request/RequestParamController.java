73546b5455dc6635ca4a903f535d6c3b7dac07d9
Dahyun Oh <odh550@naver.com>
2023-09-05 17:44:22 +0900

    HTTP 요청 메시지 - 단순텍스트

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -34,6 +34,9 @@ public class RequestParamController {
         return "ok";
     }
 
+    /**
+     * HTTP 파라미터 이름이 변수 이름과 같으면 파라미터 이름은 생략 가능
+     */
     @ResponseBody
     @RequestMapping("/request-param-v3")
     public String requestParamV3(
@@ -44,6 +47,9 @@ public class RequestParamController {
         return "ok";
     }
 
+    /**
+     * ‘String, int, Integer’ 등의 단순 타입이면 @RequestParam도 생략 가능
+     */
     @ResponseBody
     @RequestMapping("/request-param-v4")
     public String requestParamV4(String username, int age) {
@@ -55,7 +61,7 @@ public class RequestParamController {
     @RequestMapping("/request-param-required")
     public String requestParamRequired(
             @RequestParam(required = true) String username,
-            @RequestParam(required = false) int age
+            @RequestParam(required = false) Integer age
     ) {
         log.info("username={}, age={}", username, age);
         return "ok";
@@ -64,7 +70,7 @@ public class RequestParamController {
     @ResponseBody
     @RequestMapping("/request-param-default")
     public String requestParamDefault(
-            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(defaultValue = "guest") String username,
             @RequestParam(required = false, defaultValue = "-1") int age
     ) {
         log.info("username={}, age={}", username, age);
