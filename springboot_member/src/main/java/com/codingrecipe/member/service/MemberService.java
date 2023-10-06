13630e650a493adf2190acb7f68d1f00c9071a0b
Dahyun Oh <odh550@naver.com>
2023-10-06 15:06:39 +0900

    회원가입 완료

--- springboot_member/src/main/java/com/codingrecipe/member/service/MemberService.java
+++ springboot_member/src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.member.service;
 
 import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.entity.MemberEntity;
 import com.codingrecipe.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,11 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public void save(MemberDTO memberDTO) {
+        // 1. dto -> entity 변환
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        // 2. repository의 save 메서드 호출
+        // repository의 save메서드 호출 (조건. entity객체를 넘겨줘야 함)
+        memberRepository.save(memberEntity);
+        
     }
 }
