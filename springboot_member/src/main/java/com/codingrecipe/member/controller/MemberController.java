f15de82fe37123a6c144b1e19294bea12b62f3d8
Dahyun Oh <odh550@naver.com>
2023-10-06 16:38:01 +0900

    로그인

--- springboot_member/src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ springboot_member/src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -25,6 +27,24 @@ public class MemberController {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
-        return "index";
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
     }
 }
--- springboot_member/src/main/java/com/codingrecipe/member/dto/MemberDTO.java
+++ springboot_member/src/main/java/com/codingrecipe/member/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.codingrecipe.member.dto;
 
+import com.codingrecipe.member.entity.MemberEntity;
 import lombok.*;
 
 @Getter
@@ -12,4 +13,14 @@ public class MemberDTO {
     private String memberEmail;
     private String memberPassword;
     private String memberName;
+    
+    // entity -> dto 변환
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        return memberDTO;
+    }
 }
