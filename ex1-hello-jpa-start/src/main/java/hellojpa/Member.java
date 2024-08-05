d34d61e93040f4ea78352117a3127ebd0915f88e
Dapui <odh550@naver.com>
2024-08-05 23:03:35 +0900

    일대일[1:1]

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -80,6 +80,10 @@ public class Member {
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKERT_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
