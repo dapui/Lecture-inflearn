4fd789e1472e2b141dcf6dfcf770a777f2b5fd76
Oh Dahyun <odh550@kaoni.com>
2024-08-12 17:32:43 +0900

    값 타입 컬렉션

--- ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -14,15 +14,36 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("city", "street", "123-123");
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", "12345"));
+            
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "12345"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "12345"));
+
             em.persist(member);
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode()); // 불변객체로 만들면 값을 바꾸기 위해서는 새로 만들어야한다. (부작용 없음)
-            member.setHomeAddress(newAddress);
+            em.flush();
+            em.clear();
+
+            System.out.println("========== START ==========");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // homeCity -> newCity
+//            findMember.getHomeAddress().setCity("newCity");   // 잘못된 방법 (각 타입의 필드 하나만 갈아끼운다 -> 불가)
+            Address oldAddress = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode())); // 새로운 인스턴스로 갈아끼워야 함 -> 교체
+
+            // 치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+//            findMember.getAddressHistory().remove(new Address("old1", "street", "12345"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "street", "12345"));
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -4,71 +4,11 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-// SEQUENCE 전략
-//@Entity
-//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq", initialValue = 1, allocationSize = 1)
-//public class Member {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
-//    private Long id;
-//
-//    @Column(name = "name", nullable = false)
-//    private String username;
-//
-//    public Member() {}
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//}
-
-
-// TABLE 전략
-//@Entity
-//@TableGenerator(name = "member_seq_generator", table = "my_sequences", pkColumnValue = "member_seq", allocationSize = 1)
-//public class Member {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "member_seq_generator")
-//    private Long id;
-//
-//    @Column(name = "name", nullable = false)
-//    private String username;
-//
-//    public Member() {}
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//}
-
+import static jakarta.persistence.CascadeType.ALL;
 
 @Entity
 public class Member {
@@ -80,22 +20,21 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    // 기간
-    @Embedded
-    private Period workPeriod;
-
-    // 주소
     @Embedded
     private Address homeAddress;
 
-    // 주소
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name="city", column = @Column(name="WORK_CITY")),
-            @AttributeOverride(name="street", column = @Column(name="WORK_STREET")),
-            @AttributeOverride(name="zipcode", column = @Column(name="WORK_ZIPCODE"))
-    })
-    private Address worksAddress;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -113,19 +52,27 @@ public class Member {
         this.username = username;
     }
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
