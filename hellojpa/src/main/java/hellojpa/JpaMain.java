59e2b754b509121efc536e04721b75c448a4f4f1
Dapui <odh550@naver.com>
2024-08-16 22:02:25 +0900

    페이징

--- hellojpa/src/main/java/hellojpa/JpaMain.java
+++ hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -16,32 +16,27 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);  // 반환타입이 명확할 때
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");  // 반환타입이 명확하지 않을 때
+            em.flush();
+            em.clear();
 
-            List<Member> resultList = query1.getResultList();   // getResultList -> 컬렉션이 반환됨 :: 결과가 없으면 빈 리스트 반환
-            for (Member member1 : resultList) {
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                            .setFirstResult(1)
+                            .setMaxResults(10)
+                            .getResultList();
+
+            System.out.println("result.size = " + result.size());
+
+            for (Member member1 : result) {
                 System.out.println("member1 = " + member1);
             }
 
-            Member result = query1.getSingleResult();   // getSingleResult -> 반환값이 하나일 때 :: 결과가 없거나 둘 이상이면 Exception (주의!)
-
-            // 파라미터 바인딩
-//            TypedQuery<Member> query4 = em.createQuery("select m from Member m where m.username = :username", Member.class);
-//            query4.setParameter("username", "member1");
-//            Member singleResult = query4.getSingleResult();
-//            System.out.println("singleResult = " + singleResult.getUsername());
-            // 메서드 체인
-            Member result2 = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result2 = " + result2.getUsername());
 
             tx.commit();
         } catch (Exception e) {
