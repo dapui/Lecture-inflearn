dca22d02262b71b83161b3e6714cff0d37c20bc8
Oh Dahyun <odh550@kaoni.com>
2024-08-20 18:20:46 +0900

    Named 쿼리

--- hellojpa/src/main/java/hellojpa/JpaMain.java
+++ hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -42,39 +42,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // 엔티티 직접 사용 - 기본 키 값
-            // 엔티티를 파라미터로 전달
-//            String query = "select m from Member m where m = :member";
-//            Member findMember = em.createQuery(query, Member.class)
-//                    .setParameter("member", member1)
-//                    .getSingleResult();
-//            System.out.println("findMember = " + findMember);
-
-            // 식별자를 직접 전달
-//            String query = "select m from Member m where m.id = :memberId";
-//            Member findMember = em.createQuery(query, Member.class)
-//                    .setParameter("memberId", member1.getId())
-//                    .getSingleResult();
-//            System.out.println("findMember = " + findMember);
-            
-            // 엔티티 직접 사용 - 외래 키 값
-            // 엔티티를 파라미터로 전달
-//            String query = "select m from Member m where m.team = :team";
-//            List<Member> members = em.createQuery(query, Member.class)
-//                    .setParameter("team", teamA)
-//                    .getResultList();
-//
-//            for (Member member : members) {
-//                System.out.println("member = " + member);
-//            }
-
-            // 식별자를 직접 전달
-            String query = "select m from Member m where m.team.id = :teamId";
-            List<Member> members = em.createQuery(query, Member.class)
-                    .setParameter("teamId", teamA.getId())
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1")
                     .getResultList();
 
-            for (Member member : members) {
+            for (Member member : resultList) {
                 System.out.println("member = " + member);
             }
 
--- hellojpa/src/main/java/hellojpa/Member.java
+++ hellojpa/src/main/java/hellojpa/Member.java
@@ -3,6 +3,10 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
