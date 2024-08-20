310af4d4a1c6008898644c700e627c26456e80fb
Oh Dahyun <odh550@kaoni.com>
2024-08-20 14:53:32 +0900

    페치조인1-기본

--- hellojpa/src/main/java/hellojpa/JpaMain.java
+++ hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -16,50 +16,49 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            /**
-             * JPQL 기본 함수
-             * CONCAT
-             * SUBSTRING
-             * TRIM
-             * LOWER, UPPER
-             * LENGTH
-             * LOCATE
-             * ABS, SQRT, MOD
-             * SIZE, INDEX(JPA 용도)
-             */
-//            String query = "select concat('a', 'b') from Member m";
-//            String query = "select 'a' || 'b' from Member m"; // 하이버네이트에서 제공
-//            String query = "select substring(m.username, 2, 3) from Member m";
-//            String query = "select trim(m.username) from Member m";
-//            String query = "select lower(m.username) from Member m";
-//            String query = "select locate('de','abcdef') from Member m";
-//            String query = "select size(t.members) from Team t";
-//            String query = "select index(t.members) from Team t";   // 사용 안하는걸 추천
-//            String query = "select index(t.members) from Team t";
+            // 엔티티 페치 조인
+//            String query = "select m from Member m join fetch m.team";
+            // 컬렉션 페치 조인 (일대다 관계)
+            String query = "select t from Team t join fetch t.members where t.name = '팀A'";
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
 
-            // 사용자 정의 함수 호출
-//            String query = "select function('group_concat', m.username) from Member m";
-            String query = "select group_concat(m.username) from Member m"; // 하이버네이트에서 제공
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//            }
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
 
-            List<String> result = em.createQuery(query, String.class).getResultList();
-
-            for (String s : result) {
-                System.out.println("s = " + s);
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
+
 
             tx.commit();
         } catch (Exception e) {
