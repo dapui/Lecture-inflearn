e7ae532046d3fd26cbb24b85d1ef23f95ac329b6
Oh Dahyun <odh550@kaoni.com>
2024-08-20 18:42:24 +0900

    벌크연산

--- hellojpa/src/main/java/hellojpa/Member.java
+++ hellojpa/src/main/java/hellojpa/Member.java
@@ -3,10 +3,6 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@NamedQuery(
-        name = "Member.findByUsername",
-        query = "select m from Member m where m.username = :username"
-)
 public class Member {
 
     @Id
