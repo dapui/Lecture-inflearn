b9bb53afab75eb8781789255a63ef74a7d3f6950
Dapui <odh550@naver.com>
2024-08-24 21:10:05 +0900

    회원 기능 테스트

--- jpashop-v20210728/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop-v20210728/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +14,9 @@ import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
+@RunWith(SpringRunner.class)    // JUnit 실행할때 Spring이랑 엮어서 실행
+@SpringBootTest // SpringBoot를 띄운 상태로 테스트 할 경우 - 스프링 컨테이너 안에서 테스트를 실행 (없으면 @Autowired 등 실패함)
+@Transactional  // 트랜젝션을 걸고 테스트 한 다음 테스트가 끝나면 롤백 시킴
 public class MemberServiceTest {
 
     @Autowired MemberService memberService;
@@ -23,6 +24,7 @@ public class MemberServiceTest {
     @Autowired EntityManager em;
 
     @Test
+//    @Rollback(value = false)    // 롤백시키지 않고 싶을 때
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
