8e6fd003615d50b50f13df7dd6182eba2a8a0d71
Dapui <odh550@naver.com>
2024-09-01 22:49:29 +0900

    주문조회 V6: JPA에서 DTO로 직접조회, 플랫 데이터 최적화

--- jpashop-v20210728/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ jpashop-v20210728/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -26,4 +26,12 @@ public class OrderQueryDto {
         this.orderStatus = orderStatus;
         this.address = address;
     }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> value) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
 }
