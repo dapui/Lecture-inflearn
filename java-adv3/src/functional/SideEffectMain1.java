f8655be911417d663595f7210605995d6ab77202
Dapui <odh550@naver.com>
2025-06-01 11:55:29 +0900

    자바와 함수형 프로그래밍1

---
+++ java-adv3/src/functional/SideEffectMain1.java
@@ -0,0 +1,21 @@
+package functional;
+
+import java.util.function.Function;
+
+public class SideEffectMain1 {
+
+    public static int count = 0;
+
+    public static void main(String[] args) {
+        // 부수 효과(Side Effect) 최소화
+        System.out.println("before count = " + count);
+
+        Function<Integer, Integer> func = x -> {
+            count++; // 부수 효과(Side Effect)
+            return x * 2;
+        };
+        func.apply(10);
+
+        System.out.println("after count = " + count);
+    }
+}
