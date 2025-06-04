9a9318555c657a84f5652dc6521e9ed1404fef2f
Dapui <odh550@naver.com>
2025-06-04 22:18:39 +0900

    프로젝트 생성

---
+++ hell-spring-v2/src/main/java/hello/hell_spring_v2/HellSpringV2Application.java
@@ -0,0 +1,13 @@
+package hello.hell_spring_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellSpringV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellSpringV2Application.class, args);
+	}
+
+}
---
+++ hell-spring-v2/src/test/java/hello/hell_spring_v2/HellSpringV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hell_spring_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellSpringV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
