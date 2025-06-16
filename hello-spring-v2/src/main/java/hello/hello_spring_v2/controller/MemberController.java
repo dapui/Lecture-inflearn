6d16ea408140b7f0386c501748fd9279979b04e0
Dapui <odh550@naver.com>
2025-06-16 20:54:55 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ hello-spring-v2/src/main/java/hello/hello_spring_v2/controller/MemberController.java
@@ -0,0 +1,14 @@
+package hello.hello_spring_v2.controller;
+
+import hello.hello_spring_v2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- hello-spring-v2/src/main/java/hello/hello_spring_v2/service/MemberService.java
+++ hello-spring-v2/src/main/java/hello/hello_spring_v2/service/MemberService.java
@@ -3,10 +3,12 @@ package hello.hello_spring_v2.service;
 import hello.hello_spring_v2.domain.Member;
 import hello.hello_spring_v2.repository.MemberRepository;
 import hello.hello_spring_v2.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
