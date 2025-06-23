40443235c21600d3e8c9145e6cfc4c307b62adbb
Dapui <odh550@naver.com>
2025-06-24 08:14:02 +0900

    프로젝트 생성

---
+++ core-v2/src/main/java/hello/core_v2/CoreV2Application.java
@@ -0,0 +1,13 @@
+package hello.core_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreV2Application.class, args);
+	}
+
+}
