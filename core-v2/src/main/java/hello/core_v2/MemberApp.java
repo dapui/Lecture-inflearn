68f079c502cdfd6e6a12913065ce1a307d85d6da
Dapui <odh550@naver.com>
2025-06-26 22:12:59 +0900

    회원 도메인 실행과 테스트

---
+++ core-v2/src/main/java/hello/core_v2/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core_v2;
+
+import hello.core_v2.member.Grade;
+import hello.core_v2.member.Member;
+import hello.core_v2.member.MemberService;
+import hello.core_v2.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
