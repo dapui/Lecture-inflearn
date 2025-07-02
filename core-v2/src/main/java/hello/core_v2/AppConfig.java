d5a8f984146461a0a1aca72439ac4b8eefd32392
Dapui <odh550@naver.com>
2025-07-02 19:10:27 +0900

    AppConfig 리팩터링

--- core-v2/src/main/java/hello/core_v2/AppConfig.java
+++ core-v2/src/main/java/hello/core_v2/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core_v2;
 
+import hello.core_v2.discount.DiscountPolicy;
 import hello.core_v2.discount.FixDiscountPolicy;
+import hello.core_v2.member.MemberRepository;
 import hello.core_v2.member.MemberService;
 import hello.core_v2.member.MemberServiceImpl;
 import hello.core_v2.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core_v2.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
