feeac066ff76b0a1344cab356316046a1a07879a
Dapui <odh550@naver.com>
2025-07-04 22:51:14 +0900

    스프링으로 전환하기

--- core-v2/src/main/java/hello/core_v2/AppConfig.java
+++ core-v2/src/main/java/hello/core_v2/AppConfig.java
@@ -9,22 +9,29 @@ import hello.core_v2.member.MemberServiceImpl;
 import hello.core_v2.member.MemoryMemberRepository;
 import hello.core_v2.order.OrderService;
 import hello.core_v2.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
