0f24af963e080c6f8fa9397cec88424fac943a64
Dapui <odh550@naver.com>
2025-07-09 22:06:05 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- core-v2/src/main/java/hello/core_v2/discount/RateDiscountPolicy.java
+++ core-v2/src/main/java/hello/core_v2/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core_v2.discount;
 
 import hello.core_v2.member.Grade;
 import hello.core_v2.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
