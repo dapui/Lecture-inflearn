c8de3427aad3581e39647a3a432db700946f38e9
Dapui <odh550@naver.com>
2025-07-09 22:49:30 +0900

    필터

---
+++ core-v2/src/test/java/hello/core_v2/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package hello.core_v2.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ core-v2/src/test/java/hello/core_v2/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package hello.core_v2.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
