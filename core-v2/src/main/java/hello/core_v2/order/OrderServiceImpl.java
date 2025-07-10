4b4d11abf728efb07a4186e06736a665b02f07e3
Dapui <odh550@naver.com>
2025-07-10 22:34:36 +0900

    다양한 의존관계 주입 방법

--- core-v2/src/main/java/hello/core_v2/order/OrderServiceImpl.java
+++ core-v2/src/main/java/hello/core_v2/order/OrderServiceImpl.java
@@ -12,11 +12,37 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // 필드 주입 -> 사용x, 권장하지 않음. 어차피 setXxx를 만들어야 함. 테스트코드 정도에서만 사용
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+    // 수정자 주입(setter 주입)
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+
+    // 수정자 주입(setter 주입)
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        System.out.println("discountPolicy = " + discountPolicy);
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 생성자 주입
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    // 일반 메서드 주입
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- core-v2/src/test/java/hello/core_v2/scan/AutoAppConfigTest.java
+++ core-v2/src/test/java/hello/core_v2/scan/AutoAppConfigTest.java
@@ -1,7 +1,9 @@
 package hello.core_v2.scan;
 
 import hello.core_v2.AutoAppConfig;
+import hello.core_v2.member.MemberRepository;
 import hello.core_v2.member.MemberService;
+import hello.core_v2.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,5 +17,9 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
