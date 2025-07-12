a939ea11825ed4f17c257ac4f5fc3b93f422642c
Dapui <odh550@naver.com>
2025-07-12 15:46:19 +0900

    생성자 주입을 선택해라!

---
+++ core-v2/src/test/java/hello/core_v2/order/OrderServiceImplTest.java
@@ -0,0 +1,22 @@
+package hello.core_v2.order;
+
+import hello.core_v2.discount.FixDiscountPolicy;
+import hello.core_v2.member.Grade;
+import hello.core_v2.member.Member;
+import hello.core_v2.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
